6516c79108d648d27cb757902cceadcffdf92788
Marius Gheorghe <gmarius@gmail.com>
2015-01-27 18:32:34 +0200

    initial commit

---
+++ SharpQuilt/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+namespace SharpQuilt.Server
+
+open SharpQuilt.Server
+open System.Configuration
+open System
+
+module test = 
+    [<EntryPoint>]
+    let main argv = 
+
+        let reader = new AppSettingsReader()
+
+        let port : int = Convert.ToInt32(reader.GetValue("port", typedefof<Int32>))
+                                              
+        let s = Server(port)
+        s.Start()
+                                
+        0 // return an integer exit code
---
+++ SharpQuilt/Server.fs
@@ -0,0 +1,23 @@
+﻿namespace SharpQuilt.Server
+
+
+open System
+open System.Net
+open System.Collections.Generic
+
+type Server(port : int) =
+     class      
+         let source  = new Dictionary<String,string>()
+                
+         let socket = new Sockets.TcpListener(port)
+         
+         member this.Start() = 
+
+                socket.Start()
+                
+                Console.WriteLine("start server")
+            
+          member this.Stop()=
+                Console.WriteLine("stopped server")
+                
+     end
