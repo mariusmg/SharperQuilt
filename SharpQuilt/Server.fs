d16d64a57b06812ca27b1b06176d76cf95133631
Marius Gheorghe <gmarius@gmail.com>
2015-01-29 19:00:20 +0200

    more stuff

--- SharpQuilt/Server.fs
+++ SharpQuilt/Server.fs
@@ -1,23 +1,31 @@
 ﻿namespace SharpQuilt.Server
 
-
 open System
-open System.Net
+open System.Net.Sockets
 open System.Collections.Generic
+open System.Threading
 
-type Server(port : int) =
-     class      
-         let source  = new Dictionary<String,string>()
-                
-         let socket = new Sockets.TcpListener(port)
-         
-         member this.Start() = 
+type Server(port : int) = 
+    class
+        let source = new Dictionary<String, string>()
+        let thread = null
+        let socket = new TcpListener(port)
+        
+        member this.Start() = 
+            startServer
+            Console.WriteLine("start server")
+        
+        member this.Stop() = Console.WriteLine("stopped server")
 
-                socket.Start()
-                
-                Console.WriteLine("start server")
-            
-          member this.Stop()=
-                Console.WriteLine("stopped server")
-                
-     end
+
+        member private this.startServer() = 
+            thread = new Thread(fun () -> 
+                        socket.Start()
+                        while (true) do
+                            //use networkStream : NetworkStream = socket.GetStream()
+                            //                        let bytesFrom = new byte[10025]; 
+                            //                        networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize); string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);                                                                 
+             )
+            ()
+
+    end
